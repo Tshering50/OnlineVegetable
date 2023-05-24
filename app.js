3779700371d0f7a820faa1582469c815bcd57b47
Tshering-Dekar <82800013+Tshering50@users.noreply.github.com>
2023-05-24 22:00:32 +0600

    adds

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const viewRoutes = require("./router/viewsroutes");
+const dotenv = require('dotenv')
+dotenv.config({ path: './.env' })
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+const mongoose = require('mongoose')
 
 
 
 app.use("/",viewRoutes);
 
+const local_DB = process.env.DATABASE_LOCAL
+
+mongoose.connect(local_DB).then((con) => {
+  // console.log(con.connections)
+  console.log('DB local connection successful')
+}).catch(error => console.log(error));
 
 
 const port = process.env.PORT || 4001;
